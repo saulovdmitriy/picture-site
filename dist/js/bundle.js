171b317f9a335fe2dad6f4beb48da01d7f3ef8d0
Dmitriy Saulov <saulovdmitriy@gmail.com>
2019-05-26 18:57:59 +0300

    Сделал верхний слайдер

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -238,7 +238,37 @@ module.exports = sliderBottom;
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var sliderTop = function sliderTop() {};
+var sliderTop = function sliderTop() {
+  var sliderItem = document.querySelectorAll('.main-slider-item'),
+      slideIndex = 0;
+  toggleSlide(slideIndex);
+
+  function toggleSlide(n) {
+    for (var i = 0; i < sliderItem.length; i++) {
+      sliderItem[i].style.display = 'none';
+    }
+
+    if (n == 0) {
+      sliderItem[n].style.display = 'block';
+      sliderItem[n].classList.add('fadeInDown', 'animated');
+      sliderItem[n + 1].style.display = 'none';
+      sliderItem[n + 1].classList.remove('fadeInDown', 'animated');
+      slideIndex += 1;
+    }
+
+    if (n == 1) {
+      sliderItem[n - 1].style.display = 'none';
+      sliderItem[n - 1].classList.remove('fadeInDown', 'animated');
+      sliderItem[n].style.display = 'block';
+      sliderItem[n].classList.add('fadeInDown', 'animated');
+      slideIndex -= 1;
+    }
+  }
+
+  setInterval(function () {
+    toggleSlide(slideIndex);
+  }, 3000);
+};
 
 module.exports = sliderTop;
 
--- src/js/parts/slider-top.js
+++ src/js/parts/slider-top.js
@@ -1,5 +1,40 @@
 const sliderTop = () => {
     
-}
+    let sliderItem = document.querySelectorAll('.main-slider-item'),
+        slideIndex = 0;
+
+    toggleSlide(slideIndex);
+
+    function toggleSlide(n) {
+
+        for (let i = 0; i < sliderItem.length; i++) {
+            sliderItem[i].style.display = 'none';
+        }
+
+        if (n == 0) {
+            sliderItem[n].style.display = 'block';
+            sliderItem[n].classList.add('fadeInDown', 'animated');
+
+            sliderItem[n+1].style.display = 'none';
+            sliderItem[n+1].classList.remove('fadeInDown', 'animated');
+
+            slideIndex += 1;
+                
+        } if (n == 1) {
+            sliderItem[n-1].style.display = 'none';
+            sliderItem[n-1].classList.remove('fadeInDown', 'animated');
+
+            sliderItem[n].style.display = 'block';
+            sliderItem[n].classList.add('fadeInDown', 'animated');
+
+            slideIndex -= 1;
+        }        
+    }
+
+    setInterval(function() {
+        toggleSlide(slideIndex);
+    }, 3000);
+
+};
 
 module.exports = sliderTop;
