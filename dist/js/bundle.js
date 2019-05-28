b173db45bc41b04fd87bebb1cf9aad68cd65db95
Dmitriy Saulov <saulovdmitriy@gmail.com>
2019-05-28 21:49:58 +0300

    Добавил гамбургер-меню

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -260,7 +260,18 @@ module.exports = loadBlocks;
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var menu = function menu() {};
+var menu = function menu() {
+  var menuBtn = document.querySelector('.burger'),
+      menu = document.querySelector('.burger-menu'),
+      width = window.innerWidth;
+  console.log(width);
+
+  if (width <= 768) {
+    menuBtn.addEventListener('click', function () {
+      menu.classList.toggle('active');
+    });
+  }
+};
 
 module.exports = menu;
 
--- src/js/parts/menu.js
+++ src/js/parts/menu.js
@@ -1,5 +1,15 @@
 const menu = () => {
     
-}
+    let menuBtn = document.querySelector('.burger'),
+        menu = document.querySelector('.burger-menu'),
+        width = window.innerWidth;
+        console.log(width);
+
+    if (width <= 768) {
+        menuBtn.addEventListener('click', () => {
+            menu.classList.toggle('active');
+        });
+    }
+};
 
 module.exports = menu;
