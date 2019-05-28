554deae04b852124bad78255df5d8a65d5a5a4fb
Dmitriy Saulov <saulovdmitriy@gmail.com>
2019-05-28 18:31:13 +0300

    Сделал фильтр по табам портфолио

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -530,7 +530,45 @@ module.exports = sliderTop;
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var tabs = function tabs() {};
+var tabs = function tabs() {
+  var portfolioMenu = document.querySelector('.portfolio-menu'),
+      menuItem = document.querySelectorAll('.portfolio-menu li'),
+      portfolioBlock = portfolio.querySelectorAll('.portfolio-block'),
+      noPortfolio = portfolio.querySelector('.portfolio-no');
+  portfolioMenu.addEventListener('click', filterClass);
+
+  function filterClass(event) {
+    var target = event.target;
+
+    function classFilter(elementClass, noPort) {
+      if (target && event.target.classList.contains(elementClass)) {
+        menuItem.forEach(function (item) {
+          item.classList.remove('active');
+        });
+        event.target.classList.add('active');
+        portfolioBlock.forEach(function (elem) {
+          elem.style.display = 'block';
+          elem.classList.remove('fadeIn', 'animated');
+
+          if (!elem.classList.contains(elementClass)) {
+            elem.style.display = 'none';
+            noPortfolio.style.display = 'block';
+          } else {
+            elem.classList.add('fadeIn', 'animated');
+          }
+        });
+      }
+    }
+
+    classFilter('all');
+    classFilter('lovers');
+    classFilter('chef');
+    classFilter('girl');
+    classFilter('guy');
+    classFilter('grandmother');
+    classFilter('granddad');
+  }
+};
 
 module.exports = tabs;
 
