b622859ca60a9020a69dd9b8aadc2812de33c9df
Dmitriy Saulov <saulovdmitriy@gmail.com>
2019-05-29 14:53:42 +0300

    Исправил функцию изменения картинок при наведении

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1746,30 +1746,26 @@ var picSize = function picSize() {
       imgSizeTwo = document.querySelector('.size-2'),
       imgSizeThree = document.querySelector('.size-3'),
       imgSizeFore = document.querySelector('.size-4');
-  imgSizeOne.addEventListener('mouseover', function () {
-    imgSizeOne.src = 'src/img/sizes-1-1.png';
-  });
-  imgSizeTwo.addEventListener('mouseover', function () {
-    imgSizeTwo.src = 'src/img/sizes-2-1.png';
-  });
-  imgSizeThree.addEventListener('mouseover', function () {
-    imgSizeThree.src = 'src/img/sizes-3-1.png';
-  });
-  imgSizeFore.addEventListener('mouseover', function () {
-    imgSizeFore.src = 'src/img/sizes-4-1.png';
-  });
-  imgSizeOne.addEventListener('mouseout', function () {
-    imgSizeOne.src = 'src/img/sizes-1.png';
-  });
-  imgSizeTwo.addEventListener('mouseout', function () {
-    imgSizeTwo.src = 'src/img/sizes-2.png';
-  });
-  imgSizeThree.addEventListener('mouseout', function () {
-    imgSizeThree.src = 'src/img/sizes-3.png';
-  });
-  imgSizeFore.addEventListener('mouseout', function () {
-    imgSizeFore.src = 'src/img/sizes-4.png';
-  });
+
+  function imgSize(img, event, source) {
+    img.addEventListener(event, function () {
+      img.src = source;
+      img.style.zIndex = 4;
+
+      if (event == 'mouseout') {
+        img.style.zIndex = 2;
+      }
+    });
+  }
+
+  imgSize(imgSizeOne, 'mouseover', 'src/img/sizes-1-1.png');
+  imgSize(imgSizeTwo, 'mouseover', 'src/img/sizes-2-1.png');
+  imgSize(imgSizeThree, 'mouseover', 'src/img/sizes-3-1.png');
+  imgSize(imgSizeFore, 'mouseover', 'src/img/sizes-4-1.png');
+  imgSize(imgSizeOne, 'mouseout', 'src/img/sizes-1.png');
+  imgSize(imgSizeTwo, 'mouseout', 'src/img/sizes-2.png');
+  imgSize(imgSizeThree, 'mouseout', 'src/img/sizes-3.png');
+  imgSize(imgSizeFore, 'mouseout', 'src/img/sizes-4.png');
 };
 
 module.exports = picSize;
--- src/js/parts/picsize.js
+++ src/js/parts/picsize.js
@@ -5,41 +5,27 @@ const picSize = () => {
         imgSizeThree = document.querySelector('.size-3'),
         imgSizeFore = document.querySelector('.size-4');
 
+    function imgSize(img, event, source) {
+        img.addEventListener(event, () => {
+            img.src = source;
+            img.style.zIndex = 4;
+            
+            if (event == 'mouseout') {
+                img.style.zIndex = 2;
+            }
+        });    
+    }
 
+    imgSize(imgSizeOne, 'mouseover', 'src/img/sizes-1-1.png');
+    imgSize(imgSizeTwo, 'mouseover', 'src/img/sizes-2-1.png');
+    imgSize(imgSizeThree, 'mouseover', 'src/img/sizes-3-1.png');
+    imgSize(imgSizeFore, 'mouseover', 'src/img/sizes-4-1.png');
 
-    imgSizeOne.addEventListener('mouseover', () => {
-        imgSizeOne.src = 'src/img/sizes-1-1.png';
-    });
+    imgSize(imgSizeOne, 'mouseout', 'src/img/sizes-1.png');
+    imgSize(imgSizeTwo, 'mouseout', 'src/img/sizes-2.png');
+    imgSize(imgSizeThree, 'mouseout', 'src/img/sizes-3.png');
+    imgSize(imgSizeFore, 'mouseout', 'src/img/sizes-4.png');
 
-    imgSizeTwo.addEventListener('mouseover', () => {
-        imgSizeTwo.src = 'src/img/sizes-2-1.png';
-    });
-
-    imgSizeThree.addEventListener('mouseover', () => {
-        imgSizeThree.src = 'src/img/sizes-3-1.png';
-    });
-
-    imgSizeFore.addEventListener('mouseover', () => {
-        imgSizeFore.src = 'src/img/sizes-4-1.png';
-    });
-
-
-    imgSizeOne.addEventListener('mouseout', () => {
-        imgSizeOne.src = 'src/img/sizes-1.png';
-    });
-
-    imgSizeTwo.addEventListener('mouseout', () => {
-        imgSizeTwo.src = 'src/img/sizes-2.png';
-    });
-
-    imgSizeThree.addEventListener('mouseout', () => {
-        imgSizeThree.src = 'src/img/sizes-3.png';
-    });
-
-    imgSizeFore.addEventListener('mouseout', () => {
-        imgSizeFore.src = 'src/img/sizes-4.png';
-    });
-  
 };
 
 module.exports = picSize;
